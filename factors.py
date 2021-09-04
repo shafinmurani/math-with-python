492cca27c98d001488d942812aa12859a96cef1d
Shafin Murani <66113323+shafinmurani@users.noreply.github.com>
2021-09-04 19:13:17 +0530

    Update factors.py

--- factors.py
+++ factors.py
@@ -1,10 +1,10 @@
-num1 = int(input("Input the first Number : "))
-num2 = int(input("Intput the Second Number: "))
-divident = max(num1,num2)
-divisor = min(num1,num2) 
+num1 = int(input("Input the number 1 : "))
+num2 = int(input("Intput the number 2 : "))
+maxim = max(num1,num2)
+mini = mini(num1,num2) 
 i = 1
-for i in range (i, divisor+1):
-	x = divident%i
-	x1 = divisor%i
+for i in range (i, mini+1):
+	x = maxim%i
+	x1 = mini%i
 	if x == 0 and x1 == 0 :
 		print(i)
